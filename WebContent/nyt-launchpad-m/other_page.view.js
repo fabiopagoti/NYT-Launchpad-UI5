deb0d01c750c8652b229510b2685906968e26109
Fabio Pagoti <fabiopagoti@gmail.com>
2014-10-30 20:21:59 -0200

    Add another page for navigation

---
+++ WebContent/nyt-launchpad-m/other_page.view.js
@@ -0,0 +1,18 @@
+sap.ui.jsview("nyt-launchpad-m.other_page", {
+
+	/**
+	 * Specifies the Controller belonging to this View. In the case that it is
+	 * not implemented, or that "null" is returned, this View does not have a
+	 * Controller.
+	 * 
+	 * @memberOf nyt-launchpad-m.other_page
+	 */
+	getControllerName : function() {
+		return "nyt-launchpad-m.other_page";
+	},
+
+	createContent : function(oController) {
+	
+	}
+}
+
